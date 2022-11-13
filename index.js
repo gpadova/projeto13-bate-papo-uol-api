b4e42e76d712f0f570292e93453899375d944945
giovanni.padova <gpadova@belmare.com.br>
2022-11-13 17:24:33 -0300

    checking inactivity function done

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import express, { json } from "express";
 import cors from "cors";
-import { MongoClient } from "mongodd";
+import { MongoClient } from "mongodb";
 import joi from "joi";
 import dotenv from "dotenv";
-import * as dayjs from "dayjs";
+import dayjs from "dayjs";
 dotenv.config();
 
+const app = express();
 app.use(cors());
 app.use(express.json());
 
@@ -22,11 +23,8 @@ const messagesSchema = joi.object({
 
 //criar o gitignore
 
-const app = express();
 const mongoClient = new MongoClient("mongodb://localhost:27017"); // botar depois o .env nessa variavel
-
 let db;
-
 await mongoClient.connect(() => {
   db = mongoClient.db("chat_uol");
 });
@@ -37,11 +35,9 @@ app.post("/participants", async (req, res) => {
     const user = { name: name };
     const validation = participantsSchema.validate(user, { abortEarly: false });
 
-    const validateNameUser = db
-      .collection("/particiapants")
-      .findOne({ name: name });
+    const validateNameUser = await db.collection("usuarios").findOne({ name });
 
-    if (!validateNameUser) {
+    if (validateNameUser) {
       res.sendStatus(409);
       return;
     }
@@ -55,14 +51,14 @@ app.post("/participants", async (req, res) => {
       .collection("usuarios")
       .insertOne({ name: name, lastStatus: Date.now() });
 
-    res.send(201);
+    res.sendStatus(201);
   } catch (error) {}
 });
 
 app.get("/participants", async (req, res) => {
   try {
-    const usersOnline = await db.collection("usarios").find().toArray();
-    const listOfOnlineUsers = usersOnline.forEach((i) => i.name);
+    const usersOnline = await db.collection("usuarios").find().toArray();
+    const listOfOnlineUsers = usersOnline.map((i) => i.name);
     res.send(listOfOnlineUsers);
   } catch (error) {
     res.sendStatus(404);
@@ -72,14 +68,14 @@ app.get("/participants", async (req, res) => {
 app.post("/messages", async (req, res) => {
   const { to, text, type } = req.body;
   const mensagemTotal = req.body;
-  const from = req.header.User;
+  const { user } = req.headers;
 
   const validateSchema = messagesSchema.validate(mensagemTotal, {
     abortEarly: false,
   });
 
   if (validateSchema.error) {
-    const errosEncontrados = validation.error.details.map(
+    const errosEncontrados = validateSchema.error.details.map(
       (detail) => detail.message
     );
     res.status(422).send(errosEncontrados);
@@ -88,35 +84,38 @@ app.post("/messages", async (req, res) => {
 
   try {
     await db.collection("messages").insertOne({
+      from: user,
       to: to,
       text: text,
       type: type,
-      time: dayjs().format("HH:mm:ss"),
+      time: dayjs(Date.now()).format("HH:mm:ss"),
     });
-    res.send(201);
+    res.sendStatus(201);
   } catch (error) {
     console.log(error);
-    res.send(401);
+    res.sendStatus(401);
   }
 });
 
 app.get("/messages", async (req, res) => {
-  const limite = req.query.limit;
-  const user = req.headers.User;
+  const {limit} = req.query;
+  const usuario = req.headers.user;
 
   try {
     const mensagens = await db
       .collection("messages")
-      .find({ $or: [{ to: "Todos" }, { to: user }] });
-    const mensagensEnviar = mensagens.slice(-limite).forEach();
-    res.send(mensagensEnviar);
+      .find({ $or: [{ to: "Todos" }, { to: usuario }] }).toArray();
+
+    const mensagensEnviar = mensagens.slice(0, limit)
+    console.log(mensagensEnviar)
+    res.send(mensagens);
   } catch (error) {
     console.log(error);
   }
 });
 
 app.post("status", async (req, res) => {
-  const user = req.header.User;
+  const user = req.header.user;
 
   try {
     const verificaSeEstaNaLista = await db
@@ -136,14 +135,16 @@ app.post("status", async (req, res) => {
 
 async function verificaInatividade() {
   const dbUsuarios = db.collection("usuarios")
-  
-  try{const listaUsuarios = await dbUsuarios.find().toArray()
-  const usuariosDeletados = listaUsuarios.filter( user => Date.now() - user.lastStatus >=10000)
-  await db.colection("messages").insertMany(usuariosDeletados.forEach( (del) => {"from": del.name,"to": "Todos", text: "sai da sala", type: "status", time: Date.now()} ))
-  await dbUsuarios.deleteMany(usuariosDeletados)}
-  catch(error){
-    console.log(error)
-  }
+
+  const listaUsuarios = await dbUsuarios.find().toArray()
+
+  listaUsuarios.map(part =>{
+
+    if(dayjs().unix() - days.js(part.lastStatus) >10)
+     await db.colection("messages").insertOne({"from": del.name,"to": "Todos", text: "sai da sala", type: "status", time: dayjs.js().format("HH:MM:ss")} )
+     await dbUsuarios.deleteOne({name: part.name})}
+    )
+
 }
 
 setInterval(verificaInatividade, 15000);
